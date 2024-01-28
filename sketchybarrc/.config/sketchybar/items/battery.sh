dd8c3022443ddcd07e4c50faca2f57e7e1549f74
timbees <tburrey@protonmail.com>
2024-01-28 13:27:44 +0000

    alacritty updates

--- sketchybarrc/.config/sketchybar/items/battery.sh
+++ sketchybarrc/.config/sketchybar/items/battery.sh
@@ -1,4 +1,15 @@
-sketchybar --add item battery right                      \
-           --set battery script="$PLUGIN_DIR/battery.sh" \
-                         update_freq=10                  \
-           --subscribe battery system_woke
+#!/bin/bash
+
+battery=(
+  script="$PLUGIN_DIR/battery.sh"
+  icon.font="$FONT:Regular:19.0"
+  padding_right=5
+  padding_left=0
+  label.drawing=off
+  update_freq=120
+  updates=on
+)
+
+sketchybar --add item battery right      \
+           --set battery "${battery[@]}" \
+           --subscribe battery power_source_change system_woke
--- sketchybarrc/.config/sketchybar/plugins/battery.sh
+++ sketchybarrc/.config/sketchybar/plugins/battery.sh
@@ -1,28 +1,33 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-source $HOME/.config/sketchybar/icons.sh
+source "$HOME/.config/sketchybar/icons.sh"
+source "$HOME/.config/sketchybar/colors.sh"
 
-PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-CHARGING=$(pmset -g batt | grep 'AC Power')
+BATTERY_INFO="$(pmset -g batt)"
+PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
 if [ $PERCENTAGE = "" ]; then
   exit 0
 fi
 
+DRAWING=on
+COLOR=$WHITE
 case ${PERCENTAGE} in
-  9[0-9]|100) ICON=$BATTERY_100
+  9[0-9]|100) ICON=$BATTERY_100; DRAWING=off
   ;;
-  [6-8][0-9]) ICON=$BATTERY_75 
+  [6-8][0-9]) ICON=$BATTERY_75; DRAWING=off
   ;;
   [3-5][0-9]) ICON=$BATTERY_50
   ;;
-  [1-2][0-9]) ICON=$BATTERY_25
+  [1-2][0-9]) ICON=$BATTERY_25; COLOR=$ORANGE
   ;;
-  *) ICON="$BATTERY_10"
+  *) ICON=$BATTERY_0; COLOR=$RED
 esac
 
 if [[ $CHARGING != "" ]]; then
   ICON=$BATTERY_CHARGING
+  DRAWING=off
 fi
 
-sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set $NAME drawing=$DRAWING icon="$ICON" icon.color=$COLOR
