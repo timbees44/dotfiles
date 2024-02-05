89f74c95ef82e1b20552c31bb20d96679912d90c
timbees <tburrey@protonmail.com>
2024-02-05 19:18:41 +0000

    removing default keymaps for move line bug

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move Lines
+vim.keymap.del("n", "<A-j>", { desc = "Move down" })
+vim.keymap.del("n", "<A-k>", { desc = "Move up" })
+vim.keymap.del("i", "<A-j>", { desc = "Move down" })
+vim.keymap.del("i", "<A-k>", { desc = "Move up" })
+vim.keymap.del("v", "<A-j>", { desc = "Move down" })
+vim.keymap.del("v", "<A-k>", { desc = "Move up" })
