06ec861a9847c71b48e45d7038d0fb96531cf2a6
timbees <tburrey@protonmail.com>
2024-07-19 21:27:58 +0100

    moved workflows to keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,27 @@ vim.keymap.del("i", "<A-j>", { desc = "Move down" })
 vim.keymap.del("i", "<A-k>", { desc = "Move up" })
 vim.keymap.del("v", "<A-j>", { desc = "Move down" })
 vim.keymap.del("v", "<A-k>", { desc = "Move up" })
+
+-- Workflows
+-- navigate to vault
+vim.keymap.set("n", "<leader>oo", ":cd $HOME/Documents/second_brain/")
+--
+-- convert note to template and remove leading white space
+vim.keymap.set("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
+-- strip date from note title and replace dashes with spaces
+-- must have cursor on title
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
+--
+-- search for files in full vault
+vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"$HOME/Documents/second_brain/"}<cr>')
+vim.keymap.set("n", "<leader>oz", ':Telescope live_grep search_dirs={"$HOME/Documents/second_brain/"}<cr>')
+--
+-- search for files in notes (ignore zettelkasten)
+-- vim.keymap.set("n", "<leader>ois", ":Telescope find_files search_dirs={\"/Users/alex/library/Mobile\\ Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes\"}<cr>")
+-- vim.keymap.set("n", "<leader>oiz", ":Telescope live_grep search_dirs={\"/Users/alex/library/Mobile\\ Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes\"}<cr>")
+--
+-- for review workflow
+-- move file in current buffer to zettelkasten folder
+vim.keymap.set("n", "<leader>ok", ":!mv '%:p' $HOME/Documents/second_brain/zettelkasten<cr>:bd<cr>")
+-- delete file in current buffer
+vim.keymap.set("n", "<leader>odd", ":!rm '%:p'<cr>:bd<cr>")
