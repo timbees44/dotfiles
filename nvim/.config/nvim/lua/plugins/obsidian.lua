c750db8096fbc472727de73603ccc04b8093c38a
timbees <tburrey@protonmail.com>
2024-08-06 20:34:07 +0100

    fixing obsidian dailies

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -28,11 +28,11 @@ return {
       -- Optional, if you keep daily notes in a separate directory.
       folder = "daily",
       -- Optional, if you want to change the date format for the ID of daily notes.
-      date_format = "%Y-%m-%d",
+      date_format = "/%Y/%m-%B/%Y-%m-%d-%A",
       -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
       -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = "{{Date}}.md",
+      template = "Daily.md",
     },
 
     -- Optional, for templates (see below).
